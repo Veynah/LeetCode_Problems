a01a523194724449b1444184a4fabb387e8f26fe
AdamTangiev <tangiev96@gmail.com>
2023-07-08 18:55:03 +0200

    starting point

--- twosums.py
+++ twosums.py
@@ -1,1 +1,7 @@
 # https://leetcode.com/problems/two-sum/
+# First test is to understand how does the enumerate function work
+for i, value in enumerate(['apple', 'banana', 'manga']):
+    print(i, value)
+    
+
+    
