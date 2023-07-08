73b958ee591139109494681dc81ba1f57b393b50
AdamTangiev <tangiev96@gmail.com>
2023-07-09 00:22:48 +0200

    Clean version

---
+++ twosums1.1.py
@@ -0,0 +1,18 @@
+# Clean version of the two sum problem
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[n] = i
+        return
+    
