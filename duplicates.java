2503c8fa4418e9c4884f936f4648e21abc91596a
AdamTangiev <tangiev96@gmail.com>
2023-08-22 15:02:11 +0200

    Update duplicates.java

--- duplicates.java
+++ duplicates.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    public boolean contrainDuplicates(int[] nums) {
+    public boolean containsDuplicate(int[] nums) {
         Set<Integer> hashset = new HashSet<>();
 
         for (int i : nums) {
