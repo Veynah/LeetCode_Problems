9a85aad46d38e7f89b8cece151918e4da051ce58
AdamTangiev <tangiev96@gmail.com>
2023-09-07 22:07:54 +0200

    Don't like java making my files red

--- 3.validAnagram/ValidAnagram.java
+++ 3.validAnagram/ValidAnagram.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class Solution {
+public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
